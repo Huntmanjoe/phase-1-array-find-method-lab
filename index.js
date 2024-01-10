d12cc2ee1c8934e914a55025480ee63246c8de68
Hunter <Huntermatyi@gmail.com>
2024-01-10 15:51:07 -0500

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(objectArray) {
+    const win = objectArray.find(element => element.result === "W"); {
+        if (win) {
+            return win.year;
+        } else { return undefined; }
+    }
+}
+
+console.log(superbowlWin(record));
